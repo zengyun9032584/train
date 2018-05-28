b94b0cbbe96f758ce1dc4c79da7862fb82d7c7e0
zengyun <375836301@qq.com>
2018-05-28 22:53:26 +0800

    promise test
    Signed-off-by: zengyun <375836301@qq.com>

---
+++ promise.js
@@ -0,0 +1,42 @@
+function testPromise() {
+    return new Promise((resolve) => {
+        //模拟异步
+        setTimeout(() => {
+            resolve(100);
+        }, 1000);
+    })
+}
+
+testPromise().then(function (param) {
+    // 回调处理
+    console.log(param);
+})
+
+function myPromise(fn) {
+    var value = null,
+        callbacks = [];
+    this.then = (onFulfilled) => {
+        callbacks.push(onFulfilled);
+    };
+
+    function resolve(value) {
+        callbacks.forEach((callback) => {
+            callback(value);
+        });
+    }
+
+    fn(resolve);
+}
+
+function testPromise2() {
+    return new myPromise((resolve) => {
+        //模拟异步
+        setTimeout(() => {
+            resolve(1000);
+        }, 1000);
+    })
+}
+
+testPromise2().then((param) => {
+    console.log(param);
+})
